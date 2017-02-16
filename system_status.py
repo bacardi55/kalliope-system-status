3a7336e1ea4af03725c739efa7e641bed197d9f2
Raphael <raphael@khaiat.org>
2017-02-16 18:04:23 +0100

    add readme and fix bug

--- system_status.py
+++ system_status.py
@@ -11,24 +11,23 @@ logger = logging.getLogger("kalliope")
 
 class System_status(NeuronModule):
     def __init__(self, **kwargs):
-        super(SystemStatus, self).__init__(**kwargs)
+        super(System_status, self).__init__(**kwargs)
 
+        response = {}
         os, name, version, _, _, _ = platform.uname()
-        version = version.split('-')[0]
-        cores = psutil.cpu_count()
-        cpu_percent = psutil.cpu_percent()
-        memory_percent = psutil.virtual_memory()[2]
-        disk_percent = psutil.disk_usage('/')[3]
         boot_time = datetime.datetime.fromtimestamp(psutil.boot_time())
-        running_since = boot_time.strftime("%A %d. %B %Y")
-        response = "I am currently running on %s version %s.  " %(os, version)
-        response += "This system is named %s and has %s CPU cores.  " %(name, cores)
-        response += "Current CPU utilization is %s percent.  " %cpu_percent
-        response += "Current memory utilization is %s percent." %memory_percent
+        response['running_since'] = boot_time.strftime("%A %d. %B %Y")
+        response['os'] = os
+        response['os_version'] = version.split('-')[0]
+        response['system_name'] = name
+        response['system_nb_cores'] = psutil.cpu_count()
+        response['cpu'] = psutil.cpu_percent()
+        response['memory'] = psutil.virtual_memory()[2]
+        response['disk'] = psutil.disk_usage('/')[3]
 
-        print(response)
+        logger.debug(response)
 
-        self.message = response
+        self.say(response)
 
 
 
